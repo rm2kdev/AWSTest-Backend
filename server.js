cc102b9f593771ee85645b7c1cb8fb2d21c6c0c0
Ryan <rm2kdev@oneupkits.com>
2020-02-15 10:03:41 +0800

    Added buildspec dot for docker file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const MYID = uuidv1();
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send(`Connected to server: ${MYID} Hey This is a test`);
+    res.send(`Connected to server: ${MYID} Hey This is a test, but its the production branch`);
 });
 
 app.listen(PORT, HOST);
